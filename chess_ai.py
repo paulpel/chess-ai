d406a64e870dcd6722bf34b174232f28a531e783
Paul <pawelpelar@gmail.com>
2023-12-29 16:34:32 +0100

    Dragging

--- chess_ai.py
+++ chess_ai.py
@@ -19,6 +19,12 @@ DARK_SQUARE = (181, 136, 99)
 WHITE_IS_HUMAN = True
 BLACK_IS_HUMAN = True
 
+dragging = False  # Flag to track if a piece is being dragged
+dragged_piece = None  # Store the piece being dragged
+dragged_piece_pos = (0, 0)  # Current position of the dragged piece
+selected_square = None  # The starting square of the dragged piece
+
+
 # Load images
 def load_images():
     pieces = ["R", "N", "B", "Q", "K", "P"]
@@ -58,11 +64,15 @@ def draw_board(screen):
             )
 
 
-# Draw the pieces on the board
+# Draw the pieces on the board, skipping the dragged piece
 def draw_pieces(screen, images, board):
     for square in chess.SQUARES:
         piece = board.piece_at(square)
         if piece:
+            # Check if this piece is being dragged
+            if dragging and selected_square == square:
+                continue  # Skip drawing this piece in its original position
+
             symbol = piece.symbol()
             if symbol.isupper():
                 color = "w"
@@ -78,17 +88,15 @@ def draw_pieces(screen, images, board):
                 ),
             )
 
+
 # Main function
 def main():
+    global dragging, dragged_piece, dragged_piece_pos, selected_square
     screen = pygame.display.set_mode((BOARD_SIZE, BOARD_SIZE))
     pygame.display.set_caption('Chess')
     clock = pygame.time.Clock()
     images = load_images()
     board = chess.Board()
-    move_made = False  # Flag to check if a move was made
-
-    selected_piece = None  # Track the selected piece
-    valid_moves = []  # Track valid moves for the selected piece
 
     while True:
         human_turn = (board.turn and WHITE_IS_HUMAN) or (not board.turn and BLACK_IS_HUMAN)
@@ -101,28 +109,31 @@ def main():
             if human_turn:
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     selected_square = get_square_from_mouse(pygame.mouse.get_pos())
-                    selected_piece = board.piece_at(selected_square)
-                    if selected_piece and (selected_piece.color == board.turn):
-                        valid_moves = [move for move in board.legal_moves if move.from_square == selected_square]
-                elif event.type == pygame.MOUSEBUTTONUP:
+                    piece = board.piece_at(selected_square)
+                    if piece and (piece.color == board.turn):
+                        # Construct the key for the images dictionary
+                        color = 'w' if piece.color == chess.WHITE else 'b'
+                        piece_name = piece.symbol().upper() if piece.color == chess.WHITE else piece.symbol().lower()
+                        image_key = color + piece_name.upper()
+                        dragged_piece = images[image_key]  # Use the correct key to fetch the image
+                        dragged_piece_pos = pygame.mouse.get_pos()
+                        dragging = True
+
+                elif event.type == pygame.MOUSEBUTTONUP and dragging:
+                    dragging = False
                     target_square = get_square_from_mouse(pygame.mouse.get_pos())
                     move = chess.Move(selected_square, target_square)
-                    if move in valid_moves:
+                    if move in board.legal_moves:
                         board.push(move)
-                        move_made = True
-                        selected_piece = None
-                        valid_moves = []
-
-        # AI move
-        if not human_turn and move_made:
-            # AI move logic here
-            pass
-
-        if move_made:
-            move_made = False
+                    selected_square = None
 
         draw_board(screen)
         draw_pieces(screen, images, board)
+
+        # Draw the dragged piece
+        if dragging:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            screen.blit(dragged_piece, (mouse_x - SQUARE_SIZE // 2, mouse_y - SQUARE_SIZE // 2))
 
         clock.tick(FPS)
         pygame.display.flip()
